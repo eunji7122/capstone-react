9113fcabe4ed8eba59324b8bbf2e0bbce3b68222
eunji7122 <lej7122@gmail.com>
2019-10-23 15:37:39 +0900

    회원가입 부트스트랩 적용

--- src/real_estate/Register.js
+++ src/real_estate/Register.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
 import { inject } from 'mobx-react'
-import '../login.css'
+import '../vendorLogin/bootstrap/css/bootstrap.min.css'
+import '../fonts/font-awesome-4.7.0/css/font-awesome.min.css'
+import '../vendorLogin/animate/animate.css'
+import '../vendorLogin/css-hamburgers/hamburgers.min.css'
+import '../vendorLogin/select2/select2.min.css'
+import img01 from '../images/img-01.png'
+import '../css/util.css'
+import '../css/main.css'
 
 @inject('httpService')
 class Register extends React.Component {
@@ -59,7 +66,110 @@ class Register extends React.Component {
 	render() {
 		return (
 			<div>
-				<div className="login-page">
+				<div className="limiter">
+					<div className="container-login100">
+						<div className="wrap-login100">
+							<div className="login100-pic js-tilt" data-tilt>
+								<img src={img01} alt={'img01'} />
+							</div>
+							<div className="login100-form validate-form">
+								<span className="login100-form-title">Member Register</span>
+
+								<div className="wrap-input100 validate-input">
+									<input
+										className="input100"
+										type="text"
+										placeholder="username"
+										value={this.state.username}
+										onChange={this.onInputChanged}
+										name="username"
+									/>
+									<span className="focus-input100"></span>
+									<span className="symbol-input100">
+										<i className="fa fa-envelope" aria-hidden="true"></i>
+									</span>
+								</div>
+								<div
+									className="wrap-input100 validate-input"
+									data-validate="Password is required">
+									<input
+										className="input100"
+										type="password"
+										placeholder="password"
+										value={this.state.password}
+										onChange={this.onInputChanged}
+										name="password"
+									/>
+									<span className="focus-input100"></span>
+									<span className="symbol-input100">
+										<i className="fa fa-lock" aria-hidden="true"></i>
+									</span>
+								</div>
+
+								<div className="wrap-input100 validate-input">
+									<input
+										className="input100"
+										type="text"
+										placeholder="email"
+										value={this.state.email}
+										onChange={this.onInputChanged}
+										name="email"
+									/>
+									<span className="focus-input100"></span>
+									<span className="symbol-input100">
+										<i className="fa fa-envelope" aria-hidden="true"></i>
+									</span>
+								</div>
+
+								<div className="wrap-input100 validate-input">
+									<input
+										className="input100"
+										type="text"
+										placeholder="phone"
+										value={this.state.phone}
+										onChange={this.onInputChanged}
+										name="phone"
+									/>
+									<span className="focus-input100"></span>
+									<span className="symbol-input100">
+										<i className="fa fa-envelope" aria-hidden="true"></i>
+									</span>
+								</div>
+
+								<div className="wrap-input100 validate-input">
+									<input
+										className="input100"
+										type="text"
+										placeholder="private key"
+										value={this.state.private_key}
+										onChange={this.onInputChanged}
+										name="private_key"
+									/>
+									<span className="focus-input100"></span>
+									<span className="symbol-input100">
+										<i className="fa fa-envelope" aria-hidden="true"></i>
+									</span>
+								</div>
+
+								<div className="container-login100-form-btn">
+									<button className="login100-form-btn" onClick={this.register}>
+										Register
+									</button>
+								</div>
+
+								<div className="text-center p-t-136">
+									<a className="txt2" href="https://baobab.wallet.klaytn.com/">
+										Create Klaytn Wallet
+										<i
+											className="fa fa-long-arrow-right m-l-5"
+											aria-hidden="true"></i>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				{/* <div className="login-page">
 					<div className="form">
 						<div className="login-form">
 							<p>
@@ -110,7 +220,7 @@ class Register extends React.Component {
 							<button onClick={this.register}>회원가입</button>
 						</div>
 					</div>
-				</div>
+				</div> */}
 			</div>
 		)
 	}
